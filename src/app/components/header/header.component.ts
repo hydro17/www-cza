00048b4c521c6236357989967f81d5768d922e1d
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-05-14 09:35:56 +0200

    add hide/shop 'upper-advertisement' animation when clicking logo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, query } from '@angular/animations';
 
 @Component({
   selector: 'cza-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('true', style({ height: '*' })),
+      state('false', style({ height: '0px' })),
+      transition('true <=> false', [
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
+  isAdShown = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeAd() {
+    this.isAdShown = !this.isAdShown;
+  }
 }
