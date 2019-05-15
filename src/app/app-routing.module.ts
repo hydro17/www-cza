0a48e432950cc9c40307d70bfc9950a952c5c628
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-05-15 15:30:38 +0200

    add 'data.name' for all path in 'app-routing'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,34 +23,35 @@ import { ScientificArticlesDigestComponent } from './components/body/main/conten
 import { TableOfContentsComponent } from './components/body/main/content/table-of-contents/table-of-contents.component';
 import { InfoAboutCzaComponent } from './components/body/main/content/info-about-cza/info-about-cza.component';
 import { NewsComponent } from './components/body/main/content/news/news.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/news', pathMatch: 'full' },
-  { path: 'news', component: NewsComponent },
-  { path: 'info-about-cza', component: InfoAboutCzaComponent },
-  { path: 'table-of-contents', component: TableOfContentsComponent },
-  { path: 'scientific-articles-digest', component: ScientificArticlesDigestComponent },
-  { path: 'order-album-harasimowicz', component: HarasimowiczComponent },
-  { path: 'order-album-slyk', component: SlykComponent },
-  { path: 'order-album-kossuth', component: KossuthComponent },
-  { path: 'order-album-kocay', component: KocayComponent },
-  { path: 'order-album-glowniak', component: GlowniakComponent },
-  { path: 'order-album-jambor', component: JamborComponent },
-  { path: 'order-book-marszall-plynnych', component: MarszallPlynnychComponent },
-  { path: 'order-book-marszall-polstalych', component: MarszallPolstalychComponent },
-  { path: 'order-book-marszall-problemy', component: MarszallProblemyComponent },
-  { path: 'order-book-skulski', component: SkulskiComponent },
-  { path: 'order-book-slownik-jakosci-zycia', component: SlownikJakosciZyciaComponent },
-  { path: 'subscription', component: SubscriptionComponent },
-  { path: 'annual-tables-of-contents', component: AnnualTablesOfContentsComponent },
-  { path: 'rules-for-authors', component: RulesForAuthorsComponent },
-  { path: 'archival-magazines', component: ArchivalMagazinesComponent },
-  { path: 'contact-info', component: ContactInfoComponent },
-  { path: 'elf', component: ElfComponent },
-  { path: 'distinctions', component: DistinctionsComponent },
-  { path: '**', component: NewsComponent }
+  { path: 'news', component: NewsComponent, data: { name: 'news' } },
+  { path: 'info-about-cza', component: InfoAboutCzaComponent, data: { name: 'info-about-cza' } },
+  { path: 'table-of-contents', component: TableOfContentsComponent, data: { name: 'table-of-contents' } },
+  { path: 'scientific-articles-digest', component: ScientificArticlesDigestComponent, data: { name: 'scientific-articles-digest' } },
+  { path: 'order-album-harasimowicz', component: HarasimowiczComponent, data: { name: 'order-album-harasimowicz' } },
+  { path: 'order-album-slyk', component: SlykComponent, data: { name: 'order-album-slyk' } },
+  { path: 'order-album-kossuth', component: KossuthComponent, data: { name: 'order-album-kossuth' } },
+  { path: 'order-album-kocay', component: KocayComponent, data: { name: 'order-album-kocay' } },
+  { path: 'order-album-glowniak', component: GlowniakComponent, data: { name: 'order-album-glowniak' } },
+  { path: 'order-album-jambor', component: JamborComponent, data: { name: 'order-album-jambor' } },
+  { path: 'order-book-marszall-plynnych', component: MarszallPlynnychComponent, data: { name: 'order-book-marszall-plynnych' } },
+  { path: 'order-book-marszall-polstalych', component: MarszallPolstalychComponent, data: { name: 'order-book-marszall-polstalych' } },
+  { path: 'order-book-marszall-problemy', component: MarszallProblemyComponent, data: { name: 'order-book-marszall-problemy' } },
+  { path: 'order-book-skulski', component: SkulskiComponent, data: { name: 'order-book-skulski' } },
+  { path: 'order-book-slownik-jakosci-zycia', component: SlownikJakosciZyciaComponent, data: { name: 'order-book-slownik-jakosci-zycia' } },
+  { path: 'subscription', component: SubscriptionComponent, data: { name: 'subscription' } },
+  { path: 'annual-tables-of-contents', component: AnnualTablesOfContentsComponent, data: { name: 'annual-tables-of-contents' } },
+  { path: 'rules-for-authors', component: RulesForAuthorsComponent, data: { name: 'rules-for-authors' } },
+  { path: 'archival-magazines', component: ArchivalMagazinesComponent, data: { name: 'archival-magazines' } },
+  { path: 'contact-info', component: ContactInfoComponent, data: { name: 'contact-info' } },
+  { path: 'elf', component: ElfComponent, data: { name: 'elf' } },
+  { path: 'distinctions', component: DistinctionsComponent, data: { name: 'distinctions' } },
+  { path: '**', component: NewsComponent, data: { name: '**' } }
 ];
 
 @NgModule({
