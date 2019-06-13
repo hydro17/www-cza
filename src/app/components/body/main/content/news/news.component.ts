7fb7773142fc2266e24791225eeb05d3e9cc7a38
Andrzej Pepłowski <andrzej.a.peplowski@gmail.com>
2019-06-13 09:42:31 +0200

    refactor to not use ElementRef to show the enlarged gallery image

--- src/app/components/body/main/content/news/news.component.ts
+++ src/app/components/body/main/content/news/news.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent implements OnInit {
-  @ViewChild('gallerySelectionContainer') gallerySelectionContainer: ElementRef;
+  // @ViewChild('gallerySelectionContainer') gallerySelectionContainer: ElementRef;
 
   constructor() { }
 
@@ -18,16 +18,22 @@ export class NewsComponent implements OnInit {
   //   this.gallerySelectionContainer.nativeElement.classList.add('hidden');
   // }
 
-  showSelected(e) {
+  showSelected(e, gallerySelectionContainer) {
     // const selectionFrame = this.gallerySelectionFrame.nativeElement;
     // const selectionFrame = this.gallerySelectionContainer.nativeElement;
     // this.gallerySelectionContainer.nativeElement.innerHTML = e.target;
     // selectionFrame.appendChild(e.target);
 
-    this.gallerySelectionContainer.nativeElement.querySelector('div').innerHTML = `
+    // this.gallerySelectionContainer.nativeElement.querySelector('div').innerHTML = `
+    // <img src="${e.target.getAttribute('data-big-img-src')}" alt="gallery-krasnobrod-selected-image">
+    // `;
+    // this.gallerySelectionContainer.nativeElement.classList.remove('hidden');
+
+    gallerySelectionContainer.querySelector('div').innerHTML = `
     <img src="${e.target.getAttribute('data-big-img-src')}" alt="gallery-krasnobrod-selected-image">
     `;
-    this.gallerySelectionContainer.nativeElement.classList.remove('hidden');
+    gallerySelectionContainer.classList.remove('hidden');
+
     // this.gallerySelectionContainer.nativeElement.querySeletor('div').classList.remove('hidden');
 
     // selectionFrame.innerHTML = '<img src="/assets/images/galleries/kras?nobrod/krasnob-dsc0451-300px.jpg" alt="gallery-krasnobrod-1">';
